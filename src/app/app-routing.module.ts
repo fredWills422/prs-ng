a0e2e92c6ec10d23fcf842fef0b231b21ea85cef
Fred Wills <willsf422@gmail.com>
2020-02-05 20:53:25 -0500

    user list works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './feature/user/user-list/user-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: UserListComponent},
+
+  {path: 'user/list', component: UserListComponent},
+
+  {path: '**', component: UserListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './base/base.component';
+import { MenuComponent } from './core/menu/menu.component';
+import { UserListComponent } from './feature/user/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    MenuComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
