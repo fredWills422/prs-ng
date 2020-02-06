bd93aa172845e1f3da7c3cfe53b638e38c9d8796
Fred Wills <willsf422@gmail.com>
2020-02-06 01:30:23 -0500

    menu added

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'src/app/model/menu-item.class';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  menuItems: MenuItem[];
+
   constructor() { }
 
   ngOnInit() {
+    
+    this.menuItems=[
+      new MenuItem("User", "user/list", "User list"),
+      new MenuItem("Product", "product/list", "Product list"),
+      new MenuItem("Vendor", "vendor/list", "Vendor list"),
+      new MenuItem("Request", "request/list", "request list"),
+    ]
   }
 
 }
---
+++ src/app/model/menu-item.class.ts
@@ -0,0 +1,15 @@
+export class MenuItem{
+
+    display: string;
+    href: string;
+    toolTip: string;
+
+    constructor(display: string="", href: string="", toolTip: string="") {
+        
+        this.display=display;
+        this.href=href;
+        this.toolTip=toolTip;
+        
+    }
+
+}
